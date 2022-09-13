a77781fdf963ccf4805fe615917ab185763fde5d
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2022-09-13 10:32:19 -0500

    standardization updates and interval double quoting for spark

--- models/stg_stripe__payment_method_card.sql
+++ models/stg_stripe__payment_method_card.sql
@@ -4,18 +4,11 @@ with base as (
 
     select * 
     from {{ ref('stg_stripe__payment_method_card_tmp') }}
-
 ),
 
 fields as (
 
     select
-        /*
-        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
-        that are expected/needed (staging_columns from dbt_stripe_source/models/tmp/) and compares it with columns 
-        in the source (source_columns from dbt_stripe_source/macros/).
-        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
-        */
         {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_stripe__payment_method_card_tmp')),
--- models/stg_stripe__plan.sql
+++ models/stg_stripe__plan.sql
@@ -4,25 +4,17 @@ with base as (
 
     select * 
     from {{ ref('stg_stripe__plan_tmp') }}
-
 ),
 
 fields as (
 
     select
-        /*
-        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
-        that are expected/needed (staging_columns from dbt_stripe_source/models/tmp/) and compares it with columns 
-        in the source (source_columns from dbt_stripe_source/macros/).
-        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
-        */
         {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_stripe__plan_tmp')),
                 staging_columns=get_plan_columns()
             )
         }}
-        
     from base
 ),
 
@@ -33,8 +25,10 @@ final as (
         active as is_active,
         amount,
         currency,
-        {% if target.type in ('bigquery','spark') %}
+        {% if target.type == 'bigquery' %}
             `interval` as plan_interval,
+        {% if target.type == 'spark' %}
+            "interval" as plan_interval,
         {% else %}
             interval as plan_interval,
         {% endif %}
