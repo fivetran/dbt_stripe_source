1014a85ab3eb8d15b61adc9d4b181c0392ad7c76
fivetran-reneeli <renee.li@fivetran.com>
2022-11-28 14:59:19 -0600

    fix names in get paymenthod card columns

--- macros/get_payment_method_card_columns.sql
+++ macros/get_payment_method_card_columns.sql
@@ -8,10 +8,10 @@
     {"name": "payment_method_id", "datatype": dbt.type_string()},
     {"name": "type", "datatype": dbt.type_string()},
     {"name": "wallet_type", "datatype": dbt.type_string()}
-    {"name": "authentication_flow", "datatype": dbt.type_string()},
-    {"name": "result", "datatype": dbt.type_string()},
-    {"name": "result_reason", "datatype": dbt.type_string()},
-    {"name": "version", "datatype": dbt.type_string()}
+    {"name": "three_d_secure_authentication_flow", "datatype": dbt.type_string()},
+    {"name": "three_d_secure_result", "datatype": dbt.type_string()},
+    {"name": "three_d_secure_result_reason", "datatype": dbt.type_string()},
+    {"name": "three_d_secure_version", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
