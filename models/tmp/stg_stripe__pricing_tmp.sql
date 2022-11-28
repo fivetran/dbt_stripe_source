896a1333800c1e4e36db90501b2c70f468ebff11
fivetran-reneeli <renee.li@fivetran.com>
2022-11-28 14:41:02 -0600

    trying adding price and plan logic

--- models/tmp/stg_stripe__pricing_tmp.sql
+++ models/tmp/stg_stripe__pricing_tmp.sql
@@ -1,6 +1,10 @@
 {{ config(enabled=var('stripe__using_subscriptions', True)) }}
 
 select * 
-from {{ var('plan') }}
+from {% if var('stripe__price', true) %}
+{{ var('price') }}
+{% else %}
+{{ var('plan') }}
+{% endif %}
 
 {{ livemode_predicate() }}
