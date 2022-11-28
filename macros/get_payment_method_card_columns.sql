ab5b0b2fb9a776ec53a9816273f9a06d18a3ffab
fivetran-reneeli <renee.li@fivetran.com>
2022-11-28 14:53:28 -0600

    add three_d_secure obhects to payment method card

--- macros/get_payment_method_card_columns.sql
+++ macros/get_payment_method_card_columns.sql
@@ -8,6 +8,10 @@
     {"name": "payment_method_id", "datatype": dbt.type_string()},
     {"name": "type", "datatype": dbt.type_string()},
     {"name": "wallet_type", "datatype": dbt.type_string()}
+    {"name": "authentication_flow", "datatype": dbt.type_string()},
+    {"name": "result", "datatype": dbt.type_string()},
+    {"name": "result_reason", "datatype": dbt.type_string()},
+    {"name": "version", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
