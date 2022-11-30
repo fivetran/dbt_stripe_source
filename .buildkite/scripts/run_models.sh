ff70bb09fecdbd2a45f8ac8df7fcd97b1c66e731
fivetran-reneeli <renee.li@fivetran.com>
2022-11-30 16:05:23 -0600

    add vars to run sh

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -19,5 +19,5 @@ dbt deps
 dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
-dbt run --vars '{stripe__using_invoice_line_sub_filter: false, stripe__using_credit_notes: true}' --full-refresh --target "$db"
+dbt run --vars '{stripe__using_invoice_line_sub_filter: false, stripe__using_credit_notes: true, stripe__price: False, stripe__subscription_history: False}' --full-refresh --target "$db"
 dbt test --target "$db"
