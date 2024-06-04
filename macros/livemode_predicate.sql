841f53e47be5f8c3e72a94a772c9b58936a80db7
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-06-04 18:23:05 -0500

    bug/redshift-livemode-cast-to-boolean

--- macros/livemode_predicate.sql
+++ macros/livemode_predicate.sql
@@ -1,5 +1,9 @@
 {% macro livemode_predicate() %}
 
-    where cast(livemode as {{ dbt.type_boolean() }} ) = {{ var('stripe__using_livemode', true) }}
+    where 
+        cast(
+            case when cast(livemode as {{ dbt.type_string() }} ) = 'true' then true else false end 
+            as {{ dbt.type_boolean() }})
+        = {{ var('stripe__using_livemode', true) }}
 
-{% endmacro %}
+{%- endmacro %}
