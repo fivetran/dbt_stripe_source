4657edc90aed2af967f1f5cbcd3786720c19f9a6
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-10-02 15:59:29 -0700

    Releases/v0.12.latest (#83)
    * feat: add passthrough columns to card model. (#81)
    
    * feat: add non-standard columns to card model.
    
    * implement feedback from issue
    
    * Apply suggestions from code review
    
    Co-authored-by: Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
    
    * polishes
    
    * docs
    
    * explain metadata better
    
    * link
    
    * try something out
    
    * revert
    
    * changelog
    
    * test out removing code block
    
    * revert and use yaml isntead of yml
    
    ---------
    
    Co-authored-by: bramrodenburg <14278376+bramrodenburg@users.noreply.github.com>

--- models/stg_stripe__card.sql
+++ models/stg_stripe__card.sql
@@ -45,8 +45,10 @@ final as (
         source_relation
         
         {% if var('stripe__card_metadata',[]) %}
-        , {{ fivetran_utils.pivot_json_extract(string = 'metadata', list_of_properties = var('stripe__card_metadata')) }}
+        , {{ fivetran_utils.pivot_json_extract(string = 'card_metadata', list_of_properties = var('stripe__card_metadata')) }}
         {% endif %}
+
+        {{ fivetran_utils.fill_pass_through_columns('card_pass_through_columns') }}
 
     from fields
 )
