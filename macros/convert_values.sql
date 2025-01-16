77ab32ccb9c74bcf9851841f6f66c12868085b67
Renee Li <renee.li@fivetran.com>
2025-01-16 11:09:51 -0500

    update macro

--- macros/convert_values.sql
+++ macros/convert_values.sql
@@ -1,5 +1,5 @@
 {% macro convert_values(field_name, divide_by=100.0, divide_var=var('stripe__convert_values',false), alias=None) %}
-    {% if divide_var == true %}
+    {% if divide_var %}
         {{ field_name }} / {{ divide_by }} as {{ alias if alias else field_name }}
     {% else %}
         {{ field_name }} as {{ alias if alias else field_name }}
