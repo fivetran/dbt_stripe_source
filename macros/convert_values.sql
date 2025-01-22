9a3ad8d676d919f97eac7077a449642fe3ab59e6
Renee Li <renee.li@fivetran.com>
2025-01-22 13:51:22 -0500

    update macro and docs

--- macros/convert_values.sql
+++ macros/convert_values.sql
@@ -1,6 +1,6 @@
 {% macro convert_values(field_name, divide_by=100.0, divide_var=var('stripe__convert_values',false), alias=None) %}
 
-{{ adapter.dispatch('convert_values', 'stripe_source')(field_name=field_name, divide_by=divide_by, divide_var=divide_var, alias=alias ) }}
+{{ adapter.dispatch('convert_values', 'stripe_source')(field_name, divide_by, divide_var, alias ) }}
 
 {%- endmacro %}
 
