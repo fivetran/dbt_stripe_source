fc15c006551290d5627b8b74aaec41780ec975b0
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-04-01 16:57:00 -0500

    regen docs

--- models/tmp/stg_stripe__price_plan_tmp.sql
+++ models/tmp/stg_stripe__price_plan_tmp.sql
@@ -4,15 +4,6 @@
     if var('stripe__using_price', stripe_source.does_table_exist('price')=='exists') 
     else 'plan' -%}
 
-{# DEBUG #}
-{% if execute %}
-    {{print('-------- FOR DEBUGGING --------')}}
-    {{print('The value of stripe_source.does_table_exist(price) is: ' ~ stripe_source.does_table_exist('price'))}}
-    {{print('The value of stripe_source.does_table_exist(plan) is: ' ~ stripe_source.does_table_exist('plan'))}}
-    {{print('what is passed to union macro is: '~ price_or_plan)}}
-    {{print('--------')}}
-{% endif %}
-
 {{
     fivetran_utils.union_data(
         table_identifier=price_or_plan, 
